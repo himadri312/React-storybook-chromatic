ee0ce50468e09f8d3e5c7c621d83a9c13c79b1dc
Himadri Shekhar Dandapat <himadri.dandapat@oviva.com>
2022-09-22 14:23:20 +0200

    update button color

--- packages/core/src/Button/Button.styled.js
+++ packages/core/src/Button/Button.styled.js
@@ -36,14 +36,14 @@ export const Button = styled.button`
     if (variant === "primary" && !disabled) {
       return css`
         border: 1px solid black;
-        background-color: red;
+        background-color: white;
         color: black;
         padding: 20px;
       `;
     } else if (variant === "secondary" && !disabled) {
       return css`
         border: 1px solid black;
-        background-color: green;
+        background-color: white;
         color: black;
         padding: 50px;
       `;
